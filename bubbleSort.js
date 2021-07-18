86981343e891862efdb7a10907b5e876eae66c36
syd333 <sydmiranda@icloud.com>
2021-07-18 15:30:37 -0400

    helper function with bubble sort

--- bubbleSort.js
+++ bubbleSort.js
@@ -9,6 +9,7 @@
 function bubbleSort(arr) {
     for (let i = 0; i < arr.length; i++) {
         for (let j = 0; j < arr.length; j++) {
+            console.log(arr, arr[j], arr[j+1])
             if (arr[j] > arr[j+1]) {
                 //SWAP
                 let temp = arr[j];
@@ -19,6 +20,39 @@ function bubbleSort(arr) {
     }
     return arr;
 }
+
+function bubbleSort(arr) {
+    for (let i = arr.length; i > 0; i--) {
+        for (let j = 0; j < i -1; j++) {
+            console.log(arr, arr[j], arr[j+1])
+            if (arr[j] > arr[j+1]) {
+                //SWAP
+                let temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
+    return arr;
+}
+
+//ES2015
+function bubbleSort(arr) {
+    const swap = (arr, idx1, idx2) => {
+        [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
+    }
+    for (let i = arr.length; i > 0; i--) {
+        for (let j = 0; j < i -1; j++) {
+            if (arr[j] > arr[j+1]) {
+                //SWAP
+               swap(arr, j, j + 1);
+            }
+        }
+    }
+    return arr;
+}
+
+
 
 bubbleSort([37,45,29,8]);
 
