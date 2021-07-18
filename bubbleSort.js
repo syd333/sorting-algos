3199717300d62871215668be16b074159d9e8e2a
syd333 <sydmiranda@icloud.com>
2021-07-18 15:17:56 -0400

    pseduocoding

--- bubbleSort.js
+++ bubbleSort.js
@@ -1,9 +1,11 @@
 // BUBBLE SORT
-
 //sorting algo where the largest values bubble up to the top
+//PSEUDOCODE
+// start loopin from with a varable called i the end of the array towards the beginning
+// start an inner loop with a variable called j from beginning until i-1 
+// if arr[j] is greater than arr[j+1], swap these two values
 
 // SWAPING FUNCTIONALITY 
-
 function swap(arr, idx1, idx2){
     let temp = arr[idx1];
     arr[idx1] = arr[idx2];
