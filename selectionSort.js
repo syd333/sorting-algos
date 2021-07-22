1cfc926ba6b91be6d8ee7795bed1e06d9fa24231
syd333 <sydmiranda@icloud.com>
2021-07-22 09:49:18 -0400

    helper function with selectionSort

--- selectionSort.js
+++ selectionSort.js
@@ -18,13 +18,28 @@ function selectionSort(arr) {
         lowest = j;
       }
     }
-    console.log("******************");
-    console.log(arr);
-    console.log("swapping: ");
-    let temp = arr[i];
-    arr[i] = lowest;
-    arr[lowest] = temp;
-    console.log("******************");
+    if (i !== lowest) {
+      let temp = arr[i];
+      arr[i] = lowest;
+      arr[lowest] = temp;
+    }
+  }
+  return arr;
+}
+
+//ES2015
+function selectionSort(arr) {
+  const swap = (arr, idx1, idx2) => {
+    [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
+  };
+  for (let i = 0; i < arr.length; i++) {
+    let lowest = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[lowest] > arr[j]) {
+        lowest = j;
+      }
+    }
+    if (i !== lowest) swap(arr, i, lowest);
   }
   return arr;
 }
