ceaa1c6ce64f2897657364b971130430fb02c64e
syd333 <sydmiranda@icloud.com>
2021-08-26 20:46:49 -0400

    insertionSort function

--- insertionSort.js
+++ insertionSort.js
@@ -9,7 +9,15 @@
 // repeat until the array is sorted
 
 function insertionSort (arr) {
-
+    for (let i = 1; i < arr.length; i++){
+        let currentVal = arr[i];
+        for (let j = i -1; j >= 0 && arr[j] > currentVal; j--) {
+            arr[j+1] = arr[j]
+        }
+        arr[j+1] = currentVal
+    }
 
     return arr;
 }
+
+insertionSort([2,1,9,76,4])
