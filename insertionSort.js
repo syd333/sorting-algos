40d979ecbd1d6f02bda39e1232a951d6a2340243
syd333 <sydmiranda@icloud.com>
2021-08-26 20:35:41 -0400

    insertion sort pseduocode

---
+++ insertionSort.js
@@ -0,0 +1,15 @@
+// INSERTION SORT
+// builds up the sort by gradually creating a larger left half which is always sorted
+
+// PSEUDOCODE
+// start by picking the second element in the array
+// now compare the second element with the one before it & swap if necessary
+// continue to the next element and if it is in the incorrect order, iterate through
+//      the sorted portion (left side) to place the element in correct place
+// repeat until the array is sorted
+
+function insertionSort (arr) {
+
+
+    return arr;
+}
