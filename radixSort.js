507d899df6a8bad9478f8a8bda1abb1eb14f21c0
syd333 <sydmiranda@icloud.com>
2021-10-18 11:11:59 -0400

    getDigit helper function

--- radixSort.js
+++ radixSort.js
@@ -4,7 +4,17 @@
 // EXPLOITS THE FACT THAT INFO ABOUT SIZE OF A NUMBER IS ENCODED
     // IN THE NUMBER OF DIGITS
 
-// ** MORE DIGITS MEANS A BIGGER NUMBER
+// MORE DIGITS MEANS A BIGGER NUMBER
 
-// RADIX SORT HELPER METHODS
+// ** RADIX SORT HELPER METHODS
 // getDigit(num, place) - returns the digit in num @ the given place value
+
+function getDigit (num, i) {
+    return Math.floor(Math.abs(num)/ Math.pow(10, i)) % 10;
+}
+
+getDigit(7323, 2)
+// 7323/100 = 73.23 (Math.floor = 73)
+// 73 % 10 = 3
+
+
