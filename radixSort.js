2ab670c40b5778a050107f72a839be4a909b48fb
syd333 <sydmiranda@icloud.com>
2021-10-18 11:16:30 -0400

    digitcount helper function

--- radixSort.js
+++ radixSort.js
@@ -3,12 +3,11 @@
 // NEVER MAKES COMPARISONS BETWEEN ELEMENTS
 // EXPLOITS THE FACT THAT INFO ABOUT SIZE OF A NUMBER IS ENCODED
     // IN THE NUMBER OF DIGITS
-
 // MORE DIGITS MEANS A BIGGER NUMBER
 
 // ** RADIX SORT HELPER METHODS
-// getDigit(num, place) - returns the digit in num @ the given place value
 
+// getDigit(num, place) - returns the digit in num @ the given place value
 function getDigit (num, i) {
     return Math.floor(Math.abs(num)/ Math.pow(10, i)) % 10;
 }
@@ -18,3 +17,11 @@ getDigit(7323, 2)
 // 73 % 10 = 3
 
 
+// digitCount(num) - returns the number of digits in num
+function digitCount(num) {
+    if (num === 0) return 1;
+    return Math.floor(Math.log10(Math.abs(num))) + 1;
+}
+// log10 asks 10 to what power gives us this number then math.floor takes away decimal then add 1 (for actual # of digits not index)
+// math.abs takes away negative
+digitCount(423)
